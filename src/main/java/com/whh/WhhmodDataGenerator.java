ebcc1cd13b32251ce433c4de569d2a7076e46d39
whh <18330079989@163.com>
2025-08-30 00:28:52 +0800

    test

---
+++ src/main/java/com/whh/WhhmodDataGenerator.java
@@ -0,0 +1,11 @@
+package com.whh;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class WhhmodDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
